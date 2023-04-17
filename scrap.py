9426395f2f7f8524dfaee2d06e8c6100d1d725ec
Anurag <96662574+ANURAG-2043@users.noreply.github.com>
2023-04-17 23:05:12 +0530

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.message import EmailMessage
+import time
+
+email_id = 'abc.20.23.mp@gmail.com'
+email_pass = 'csxrjumclmzfpfmb'
+
+def check_price ():
+    URL = "https://www.amazon.in/Apple-iPhone-14-128GB-Blue/dp/B0BDK62PDX/ref=sr_1_5?crid=215WNQTTSDE8&keywords=iphone%2B14&qid=1681734368&sprefix=iphone%2Caps%2C204&sr=8-5&th=1"
+    headers = {"user-Agents" : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15'  }
+
+
+    page = requests.get(URL , headers = headers)
+    soup = BeautifulSoup(page.content , 'html.parser')
+    
+
+    title =soup.find(id = "productTitle").get_text()
+    
+    price = soup.find(class_ = "a-price-whole").get_text()
+    converted_price = int(float(price[0:8].replace(",","")))
+
+    print(title.strip())
+    print(converted_price) 
+    
+
+    if(converted_price <= 71999):
+        send_email()
+    else:
+        print("Price is not dropped")
+        print("No email sent")
+
+        
+ 
+def send_email():
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()  # stablishing connection with the server and the gmail
+        server.starttls()
+        server.ehlo()
+
+        server.login(email_id, email_pass)
+
+        subject = " Hey The Price Fall!! "
+        body = " Check the Amazon link https://www.amazon.in/Apple-iPhone-14-128GB-Blue/dp/B0BDK62PDX/ref=sr_1_5?crid=215WNQTTSDE8&keywords=iphone%2B14&qid=1681734368&sprefix=iphone%2Caps%2C204&sr=8-5&th=1"
+
+        msg = f"Subject : { subject}\n\n{body}"
+
+        server.sendmail(email_id, 'kumbhareanurag1023@gmail.com', msg )
+        
+        print('HEY EMAIL HAS BEEN SENT!')
+        server.quit()  # quit the server
+    
+
+# while True:
+check_price()
+time.sleep(2)
+    
+
+# def send_mail():
+#     msg = EmailMessage()
+#     msg['Subject'] = 'Product price fell down'
+#     msg='Hey buddy Check this amazon link : https://www.amazon.in/Apple-iPhone-14-128GB-Blue/dp/B0BDK62PDX/ref=sr_1_5?crid=215WNQTTSDE8&keywords=iphone%2B14&qid=1681734368&sprefix=iphone%2Caps%2C204&sr=8-5&th=1'
+#     msg['From'] = email_id
+#     msg['To'] = "kumbhareanurag1023@gmail.com"
+
+    
+#     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+#         smtp.login(email_id, email_pass)
+#         smtp.send_message(msg)
+        
+# while True:
+#     check_price ()
+#     time.sleep(10)
+
